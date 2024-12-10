6ab2dcdb61bb6e21e1bd8c3dbc5650066235b61b
Milinda12345 <37358831+Milinda12345@users.noreply.github.com>
2024-12-10 23:30:35 +0530

    abstract class

---
+++ OOp Concept/Java Abstract/Abstract Class/src/Main.java
@@ -0,0 +1,11 @@
+public class Main extends sample {
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.test1();
+        obj.test2();
+
+    }
+    void test1(){
+        System.out.println("test1");
+    }
+}
---
+++ OOp Concept/Java Abstract/Abstract Class/src/sample.java
@@ -0,0 +1,7 @@
+abstract public class sample {
+
+    abstract void test1 ();
+    void test2(){
+        System.out.println("test2");
+    }
+}
